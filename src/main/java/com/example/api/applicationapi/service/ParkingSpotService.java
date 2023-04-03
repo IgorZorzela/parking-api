1eb13a9adcf95f12e5c4d8cadb8d296caf36d8fe
Igor Zorzela <igorzorzela@gmail.com>
2023-04-03 19:45:04 -0300

    Get by id

--- src/main/java/com/example/api/applicationapi/service/ParkingSpotService.java
+++ src/main/java/com/example/api/applicationapi/service/ParkingSpotService.java
@@ -5,6 +5,10 @@ import com.example.api.applicationapi.repository.ParkingSpotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import javax.transaction.Transactional;
 
 @Service
@@ -30,6 +34,14 @@ public class ParkingSpotService {
 
     public boolean existsByApartmentAndBlock(String apartment, String block) {
         return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
+    
+    public List<ParkingSpotModel> findAll() {
+        return parkingSpotRepository.findAll();
+    }
+
+    public Optional<ParkingSpotModel> findById(UUID id) {
+        return parkingSpotRepository.findById(id);
     }
 
 }
