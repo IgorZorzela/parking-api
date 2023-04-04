d6696c2872ea44e8231bd7e588f78b9c6fe8c910
Igor Zorzela <igorzorzela@gmail.com>
2023-04-03 21:10:55 -0300

    improvements

--- src/main/java/com/example/api/applicationapi/controller/ParkingSpotController.java
+++ src/main/java/com/example/api/applicationapi/controller/ParkingSpotController.java
@@ -5,6 +5,10 @@ import com.example.api.applicationapi.model.ParkingSpotModel;
 import com.example.api.applicationapi.service.ParkingSpotService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -49,8 +53,9 @@ public class ParkingSpotController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ParkingSpotModel>> getAllParkingSpots(){
-        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll());
+    //paginação
+    public ResponseEntity<Page<ParkingSpotModel>> getAllParkingSpots(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll(pageable));
     }
 
     @GetMapping("/{id}")
--- src/main/java/com/example/api/applicationapi/service/ParkingSpotService.java
+++ src/main/java/com/example/api/applicationapi/service/ParkingSpotService.java
@@ -3,6 +3,8 @@ package com.example.api.applicationapi.service;
 import com.example.api.applicationapi.model.ParkingSpotModel;
 import com.example.api.applicationapi.repository.ParkingSpotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -36,8 +38,8 @@ public class ParkingSpotService {
         return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
     }
     
-    public List<ParkingSpotModel> findAll() {
-        return parkingSpotRepository.findAll();
+    public Page<ParkingSpotModel> findAll(Pageable pageable) {
+        return parkingSpotRepository.findAll(pageable);
     }
 
     public Optional<ParkingSpotModel> findById(UUID id) {
